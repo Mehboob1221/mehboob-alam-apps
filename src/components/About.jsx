f94d2379468d4696b550696b6f336f75f665ee5e
Your Name <your_email@example.com>
2026-08-04 19:01:46 +0500

    Added final styling to profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
       transition={{ duration: 0.8 }}
     >
       <div className="about-image">
-        <img src="/profile.jpg" alt="Mehboob Alam" style={{ width: '250px', height: '250px', borderRadius: '50%', border: '4px solid #ffc107', display: 'block', margin: '0 auto' }} />
+        <img src="/profile.jpg" alt="Mehboob Alam" className="profile-image" style={{ width: '100%', maxWidth: '300px', borderRadius: '50%' }} />
       </div>
 
       <div className="about-text">
